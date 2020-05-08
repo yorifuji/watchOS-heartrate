6e16a06d7a375b6466146eda35d0bb99aa7c059a
yorifuji <yorifuji@gmail.com>
2020-05-08 23:55:26 +0900

    Initial Commit

---
+++ watchOS-heartrate WatchKit Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  watchOS-heartrate WatchKit Extension
+//
+//  Created by yorifuji on 2020/05/08.
+//  Copyright © 2020 yorifuji. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
